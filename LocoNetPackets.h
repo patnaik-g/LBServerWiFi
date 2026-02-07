4d03a7896f4169b331c9e75e990ac431d86898e0
patnaik-g <75448582+patnaik-g@users.noreply.github.com>
2026-02-07 16:31:39 -0500

    Fix Global Power opcodes to match LocoNet standard

--- LocoNetPackets.h
+++ LocoNetPackets.h
@@ -1,25 +1,13 @@
-/*
- * @file LocoNetPackets.h
- * @brief Shared LocoNet Protocol Definitions
- *
- * PURPOSE:
- * This file acts as a "Common Block" for static packet definitions.
- * It allows unrelated modules (ActivityMonitor, PowerLine) to use the 
- * same constant packet structures without being coupled to the NetworkInterface.
- */
-
 #ifndef LOCONET_PACKETS_H
 #define LOCONET_PACKETS_H
 
 #include <LocoNetStreamESP32.h>
 
-// OPC_GPOFF (0x83) - Global Power OFF
-// Used by: ActivityMonitor (Timeout), PowerLine (Button Press)
-static const lnMsg PACKET_GP_OFF = { .data = { 0x83, 0x7C } };
+// OPC_GPOFF (0x82) - Global Power OFF
+static const lnMsg PACKET_GP_OFF = { .data = { 0x82, 0x7D } };
 
-// OPC_GPON (0x82) - Global Power ON
-// Used by: PowerLine (Button Press)
-static const lnMsg PACKET_GP_ON  = { .data = { 0x82, 0x7D } };
+// OPC_GPON (0x83) - Global Power ON
+static const lnMsg PACKET_GP_ON  = { .data = { 0x83, 0x7C } };
 
 // OPC_IDLE (0x85) - Force Idle State
 static const lnMsg PACKET_IDLE   = { .data = { 0x85, 0x7A } };
