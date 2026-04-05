58809e5009e4e7a858aac8353ce39cdebd84f678
patnaik-g <75448582+patnaik-g@users.noreply.github.com>
2026-04-05 15:37:34 -0400

    Added checksum test to incoming Loconet packets

--- LBServerWiFi.ino
+++ LBServerWiFi.ino
@@ -1,5 +1,5 @@
 /**
- * LBServerWiFi v2.5.3 - Main Orchestrator
+ * LBServerWiFi - Main Orchestrator
  */
 
 #include "Config.h"
@@ -15,15 +15,19 @@ LocoNetBus bus;
 LocoNetDispatcher parser(&bus);
 LocoNetStreamESP32 lnStream(1, PIN_LOCONET_RX, PIN_LOCONET_TX, false, true, &bus);
 
+#ifdef SYSTEM_POWER_CONTROL
 volatile bool g_SystemPower = false;
+#endif
 volatile bool g_TrackPower = false;
+uint32_t lastTrafficMilli = 0;
+
 QueueHandle_t lnToNetQueue = NULL;
 QueueHandle_t netToLnQueue = NULL;
 
 WiFiManager wifiManager(DEFAULT_HOSTNAME, DEFAULT_PORT);
-PowerLine powerMonitor;
 
 #ifdef SYSTEM_POWER_CONTROL
+PowerLine powerMonitor;
 ActivityMonitor watchdog(TIMEOUT_TRACK_MS, TIMEOUT_SYSTEM_MS);
 KasaPlug* systemPlug = NULL;
 #else
@@ -32,16 +36,13 @@ ActivityMonitor watchdog(TIMEOUT_TRACK_MS);
 
 void setup() {
     btStop();
-
-    #ifdef ENABLE_SERIAL_LOGGING
+#ifdef ENABLE_SERIAL_LOGGING
     Serial.begin(SERIAL_BAUD_RATE);
     unsigned long start = millis();
-    while (!Serial && millis() - start < 2000) { vTaskDelay(pdMS_TO_TICKS(10));
-    }
-    #endif
+    while (!Serial && millis() - start < 2000) { vTaskDelay(pdMS_TO_TICKS(10)); }
+#endif
     
     pinMode(PIN_STATUS_LED, OUTPUT);
-    
 #ifdef SYSTEM_POWER_CONTROL
     pinMode(PIN_LOCONET_RX, INPUT_PULLUP);
     pinMode(PIN_LOCONET_TX, OUTPUT);
@@ -49,14 +50,18 @@ void setup() {
 #endif
     
     Debug::begin();
-
     lnToNetQueue = xQueueCreate(LOCONET_QUEUE_DEPTH, sizeof(lnMsg));
     netToLnQueue = xQueueCreate(LOCONET_QUEUE_DEPTH, sizeof(lnMsg));
 
     wifiManager.begin();
     xTaskCreatePinnedToCore(communicationTask, "Comm", 4096, NULL, 1, NULL, 0);
     parser.onPacket(CALLBACK_FOR_ALL_OPCODES, [](const lnMsg *p) {
+        if (!validateChecksum(p)) return;
         if (p->data[0] == 0x81 && p->data[1] == 0x7E) return;
+        
+        digitalWrite(PIN_STATUS_LED, HIGH);
+        lastTrafficMilli = millis();
+
         watchdog.inspect(p);
         xQueueSend(lnToNetQueue, p, 0); 
     });
@@ -65,31 +70,23 @@ void setup() {
 
 #ifdef SYSTEM_POWER_CONTROL
     LOG_DEBUG("Scanning for Kasa Plug '%s'...\n", KASA_SMARTPLUG_NAME);
-    
     systemPlug = KasaPlug::Find(KASA_SMARTPLUG_NAME);
     if (systemPlug) {
         LOG_DEBUG("Kasa Plug Found: %s\n", systemPlug->ip);
     } else {
         LOG_DEBUG("Kasa Plug Not Found.\n");
     }
-    
     watchdog.begin(&lnStream, lnToNetQueue, systemPlug);
+    powerMonitor.begin(PIN_POWER_MONITOR, &lnStream);
 #else
     watchdog.begin(&lnStream, lnToNetQueue);
 #endif
 
-#ifdef SYSTEM_POWER_CONTROL
-    powerMonitor.begin(PIN_POWER_MONITOR, &lnStream);
-#else
-    powerMonitor.begin(PIN_POWER_MONITOR);
-#endif
-    
     LOG_DEBUG("%s initialized\n", BRIDGE_VERSION);
 }
 
 void loop() {
     if (watchdog.isSystemOff()) {
-        // OPTIMIZATION: Deep sleep the loop when system is OFF.
         vTaskDelay(pdMS_TO_TICKS(250));
         return;
     }
@@ -100,14 +97,14 @@ void loop() {
     bool worked = false;
     if (xQueueReceive(netToLnQueue, &tx, 0) == pdPASS) {
         lnStream.send(&tx);
+        digitalWrite(PIN_STATUS_LED, HIGH);
+        lastTrafficMilli = millis();
         xQueueSend(lnToNetQueue, &tx, 0);
         watchdog.inspect(&tx);
         worked = true;
     }
 
     watchdog.manage();
-    
-    // OPTIMIZATION: Yield if no packet was processed to cool CPU
     if (!worked) {
         vTaskDelay(pdMS_TO_TICKS(1));
     }
